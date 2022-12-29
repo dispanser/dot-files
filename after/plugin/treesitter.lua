2b8f830704abc0a098739b375fb988cc74d054ff
Thomas Peiselt <pi@kulturguerilla.org>
2022-12-29 14:32:02 +0100

    Add incremental selection to treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -10,13 +10,19 @@ require'nvim-treesitter.configs'.setup {
   -- List of parsers to ignore installing (for "all")
   ignore_install = { "javascript" },
 
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
     additional_vim_regex_highlighting = false,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<CR>", -- set to `false` to disable one of the mappings
+      node_incremental = "<CR>",
+      scope_incremental = "<C-s>",
+      node_decremental = "<BS>",
+    },
   },
 }
 
