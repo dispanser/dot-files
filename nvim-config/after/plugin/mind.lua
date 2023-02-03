9250fd23b61cd586c1e793770b515490b5a12adf
Thomas Peiselt <pi@kulturguerilla.org>
2023-02-03 21:58:56 +0100

    Remove mind
    I prefer to put my notes into a proper folder tree, that way it also
    works with obsidian or basically anything that can read a folder
    structure.

--- nvim-config/after/plugin/mind.lua
+++
@@ -1,3 +0,0 @@
-mind = require('mind')
-vim.keymap.set('n', '<leader>pn', mind.open_project, { desc = "open project notes" } )
-vim.keymap.set('n', '<leader>pc', mind.close, { desc = "close project notes" } )
--- nvim-config/lua/pi/packer.lua
+++ nvim-config/lua/pi/packer.lua
@@ -76,14 +76,6 @@ return require('packer').startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
-  use {
-    'phaazon/mind.nvim',
-    branch = 'v2.2',
-    requires = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      require'mind'.setup()
-    end
-  }
   use {'stevearc/dressing.nvim'}
 end)
 
