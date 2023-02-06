d91cc33eddfd18d1cf6e42539ee4d80935246113
Thomas Peiselt <pi@kulturguerilla.org>
2023-02-06 12:08:55 +0100

    env variable and shortcut for switching to a project dir

--- scripts/browser.sh
+++ scripts/browser.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i fish -p fish wmctrl
 
-# ACTIVE=$(wmctrl -d | grep -v  '  - DG:')
-set ACTIVE (wmctrl -d | grep '\*' | cut -b 33- | cut -f 1 -d_)
+# PROJECT=$(wmctrl -d | grep -v  '  - DG:')
+set PROJECT (wmctrl -d | grep '\*' | cut -b 33- | cut -f 1 -d_)
 
-qutebrowser --qt-arg name $ACTIVE --basedir  /home/pi/projects/$ACTIVE/.qute $argv
-# firefox -P $ACTIVE --class firefox_$ACTIVE --profile /home/pi/projects/$ACTIVE/.firefox --new-tab $argv
+qutebrowser --qt-arg name $PROJECT --basedir  /home/pi/projects/$PROJECT/.qute $argv
+# firefox -P $PROJECT --class firefox_$PROJECT --profile /home/pi/projects/$PROJECT/.firefox --new-tab $argv
