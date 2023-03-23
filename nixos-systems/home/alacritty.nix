862373404d8ee2ca6830bae0e75ff803782bff9a
Thomas Peiselt <pi@kulturguerilla.org>
2023-03-23 09:44:18 +0100

    Add more alt-level key bindings to alacritty
    This is for addressing tmux panes

--- nixos-systems/home/alacritty.nix
+++ nixos-systems/home/alacritty.nix
@@ -27,6 +27,18 @@
         { key = "B"; mods = "Alt"; chars = "\\x1bb"; }
         { key = "F"; mods = "Alt"; chars = "\\x1bf"; }
         { key = "D"; mods = "Alt"; chars = "\\x1bd"; }
+
+        { key = "Caret"; mods = "Alt"; chars = "\\x1b0"; }
+        { key = "Key0"; mods = "Alt"; chars = "\\x1b0"; }
+        { key = "Key1"; mods = "Alt"; chars = "\\x1b1"; }
+        { key = "Key2"; mods = "Alt"; chars = "\\x1b2"; }
+        { key = "Key3"; mods = "Alt"; chars = "\\x1b3"; }
+        { key = "Key4"; mods = "Alt"; chars = "\\x1b4"; }
+        { key = "Key5"; mods = "Alt"; chars = "\\x1b5"; }
+        { key = "Key6"; mods = "Alt"; chars = "\\x1b6"; }
+        { key = "Key7"; mods = "Alt"; chars = "\\x1b7"; }
+        { key = "Key8"; mods = "Alt"; chars = "\\x1b8"; }
+        { key = "Key9"; mods = "Alt"; chars = "\\x1b9"; }
         { key = "Period"; mods = "Alt"; chars = "\\x1b."; }
         { key = "Period"; mods = "Alt|Shift"; chars = "\\x1b>"; }
       ];
