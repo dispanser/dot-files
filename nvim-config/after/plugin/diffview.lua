4c8b6921ab6dca908b2d2eadc617eb50d7fd96ff
Thomas Peiselt <pi@kulturguerilla.org>
2023-08-10 11:40:09 +0200

    Improve diffview keybindings
    - dc: close view
    - df: open file history for current file only
    - dh: open project (repository) history

--- nvim-config/after/plugin/diffview.lua
+++ nvim-config/after/plugin/diffview.lua
@@ -1,1 +1,4 @@
-vim.keymap.set('n', '<leader>dv', vim.cmd.DiffviewOpen, { desc = "diff view"} )
+vim.keymap.set('n', '<leader>dv', vim.cmd.DiffviewOpen, { desc = "open diff view"} )
+vim.keymap.set('n', '<leader>dc', vim.cmd.DiffviewClose, { desc = "close diff view"} )
+vim.keymap.set('n', '<leader>df', function() vim.cmd.DiffviewFileHistory('%') end, { desc = "open file history diff view"} )
+vim.keymap.set('n', '<leader>dh', vim.cmd.DiffviewFileHistor, { desc = "open project history diff view"} )
