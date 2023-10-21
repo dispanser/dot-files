173b62eac1ff63005c916440fb4d75bd010e4545
Thomas Peiselt <pi@kulturguerilla.org>
2023-10-21 10:09:47 +0200

    [x12/touch] Add touch keyboard config and gesture

--- nixos-systems/home/lisgd.nix
+++ nixos-systems/home/lisgd.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   systemd.user.services.lisgd = let
-    ROT = "1";
+    ROT = "3";
     xdt = "${pkgs.xdotool}/bin/xdotool";
   in {
     Unit = {
@@ -22,6 +22,8 @@
         -g "1,RL,R,S,R,${xdt} key ctrl+l" \
         -g "1,UD,T,S,R,${pkgs.light}/bin/light -A 10" \
         -g "1,DU,B,S,R,${pkgs.light}/bin/light -U 10" \
+        -g "2,UD,N,S,R,${pkgs.psmisc}/bin/killall -r onboard" \
+        -g "2,DU,N,S,R,${pkgs.onboard}/bin/onboard -l /home/pi/src/github/dispanser/dot-files/configs/onboard/mine.onboard &" \
         -o ${ROT}
       ''}";
     };
