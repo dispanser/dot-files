43586a81856499328ddbedf880e539744ade9448
Thomas Peiselt <pi@kulturguerilla.org>
2024-01-11 09:10:50 +0100

    [scripts] fix umount script to luksClose the right thing

--- scripts/,umount-mnt.sh
+++ scripts/,umount-mnt.sh
@@ -1,4 +1,4 @@
 DEV_NAME=$1
 mount | rg --color=auto mnt | cut -f 3 -d' ' | sort -r | xargs -n 1 sudo umount
 sudo vgchange -an $DEV_NAME
-sudo cryptsetup luksClose yuking
+sudo cryptsetup luksClose $DEV_NAME
