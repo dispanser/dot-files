51bad8a5afee04464af9be6741e9b264e92b77e7
Thomas Peiselt <pi@kulturguerilla.org>
2024-01-17 09:44:01 +0100

    [xrandr] re-arrange monitors on desk 2

--- scripts/xrandr.sh
+++ scripts/xrandr.sh
@@ -41,7 +41,7 @@ function undock() {
 function dock() {
 	case ${#EXT[@]} in
 		"1")
-			xrandr --output ${EXT[0]} --auto --right-of $INT --rotate normal --output $INT --auto
+			xrandr --output ${EXT[0]} --auto --above $INT --rotate normal --output $INT --auto
 			;;
 		"2")
 			xrandr --output ${EXT[0]} --auto --output ${EXT[1]} --auto --right-of ${EXT[0]} --output $INT --off
