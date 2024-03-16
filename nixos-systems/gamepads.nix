87d86d77cbc989774f7b7fd87ff8acfeef1b72ad
Thomas Peiselt <pi@kulturguerilla.org>
2024-03-16 13:53:32 +0100

    [epiphany] enable gamepad support

---
+++ nixos-systems/gamepads.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+{
+  services.udev = {
+    enable = true; # explicit, even though that's the default
+    extraRules = ''
+      KERNEL=="uinput", MODE="0660", GROUP="pgriffais", OPTIONS+="static_node=uinput"
+    '';
+  };
+}
