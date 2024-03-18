4ca7c962b0958b09fc63499c89b4c0362f9699b0
Your Name <name@coralogix.com>
2024-03-18 17:13:29 +0100

    [nix/darwin] Make nix-darwin + home manager work again

---
+++ nixos-systems/nix-darwin/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Example Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = inputs@{ self, nix-darwin, home-manager, nixpkgs }:
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#yukon
+    darwinConfigurations."yukon" = nix-darwin.lib.darwinSystem {
+      modules = [
+        ./darwin.nix
+	home-manager.darwinModules.home-manager
+	{
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users."thomas.peiselt" = import ../home/home.nix;
+
+          # Optionally, use home-manager.extraSpecialArgs to pass
+          # arguments to home.nix
+        }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."yukon".pkgs;
+  };
+}
