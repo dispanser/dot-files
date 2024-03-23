338d1e906573423823b54fbaf3477e5af724f42e
Thomas Peiselt <pi@kulturguerilla.org>
2024-03-23 16:11:31 +0100

    [nvim] Enable neoclip
    <space>ly (latest yanks)

--- nvim-config/lua/plugins/telescope.lua
+++ nvim-config/lua/plugins/telescope.lua
@@ -1,7 +1,9 @@
 return {
-    'nvim-telescope/telescope.nvim', branch = '0.1.x',
--- or                              , tag = '0.1.3',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-      lazy = false
+  'nvim-telescope/telescope.nvim', branch = '0.1.x',
+  dependencies = { 
+    'nvim-lua/plenary.nvim',
+    'AckslD/nvim-neoclip.lua',
+  },
+  lazy = false
 }
 
