2d457d41ed906ddff3f7bd3de0652ab4a4b7b1fe
Thomas Peiselt <pi@kulturguerilla.org>
2025-12-07 18:24:24 +0100

    chatham clearing

--- nixos-systems/chatham.nix
+++ nixos-systems/chatham.nix
@@ -8,104 +8,63 @@
     ./security.nix
     ./flakes.nix
     ./adguard_dns.nix
-	  ./brother.nix
+    ./brother.nix
     ./yubico.nix
   ];
-
-  services.undervolt = {
-    temp = 90;
-    enable = false;
-    useTimer = true;
-    uncoreOffset = -50;
-    gpuOffset = -45;
-    coreOffset = -75;
-    analogioOffset = -50;
-    p1 = {
-      limit = 65;
-      window = 300;
-    };
-    p2 = {
-      limit = 90;
-      window = 224;
-    };
-  };
 
   virtualisation.docker.enable = true;
 
   services.espanso.enable = true;
-  services.atd.enable   = true;
+  services.atd.enable = true;
   services.fwupd.enable = true;
   security.pam.u2f.enable = true;
 
   hardware.enableAllFirmware = true;
   hardware.enableRedistributableFirmware = true;
 
-  hardware.firmware = with pkgs; [ 
-    wireless-regdb 
+  hardware.firmware = with pkgs; [
+    wireless-regdb
   ];
-
-  services.cron = {
-	  enable         = true;
-	  systemCronJobs = [
-	      "17 10,21 * * * pi /home/pi/bin/backup-home.sh local"
-	      "17 12 * * * pi /home/pi/bin/backup-home.sh backblaze"
-        "17 11,18 * * * pi /home/pi/bin/backup-home.sh nextcloud"
-        # "*/15 * * * * root /run/current-system/sw/bin/cpupower frequency-set -g ondemand"
-    ];
-  };
 
   services.blueman.enable = true;
 
   hardware.bluetooth = {
-    enable          = true;
-    package         = pkgs.bluez;
+    enable = true;
+    package = pkgs.bluez;
   };
 
   security.rtkit.enable = true;
 
   hardware.cpu.intel.updateMicrocode = true;
 
-  virtualisation.virtualbox = {
-    host.enable = false;
-    # Enable the Oracle Extension Pack.
-    host.enableExtensionPack = true;
-  };
-
-# programs.slock.enable       = true;
-  # services.physlock.enable                      = true;
-
-	  boot = {
-      initrd.luks = {
-        fido2Support = false;
-        devices = {
-          nvme = {
-            preLVM = true;
-            device = "/dev/disk/by-uuid/7a3362b0-cde4-4749-ab9f-08a6b07e8412";
-          };
+  boot = {
+    initrd.luks = {
+      fido2Support = false;
+      devices = {
+        nvme = {
+          preLVM = true;
+          device = "/dev/disk/by-uuid/7a3362b0-cde4-4749-ab9f-08a6b07e8412";
         };
-      }; 
-		  loader.systemd-boot.enable = true;
-		  loader.efi.canTouchEfiVariables = true;
-		  initrd.availableKernelModules   = [ "xhci_pci" "uas" "usbhid" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
-		  initrd.kernelModules            = [ "xhci_pci" "uas" "usbhid" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
-		  kernelModules                   = [ "tp_smapi" "acpi_call" ];
-		  extraModulePackages             = [ config.boot.kernelPackages.tp_smapi config.boot.kernelPackages.acpi_call ];
-		  extraModprobeConfig = ''
-			  options acpi ec_no_wakeup=1
-			  options thinkpad_acpi fan_control=1
-		  '';
-		  kernelPackages     = pkgs.linuxPackages_latest;
-	  };
-
-  powerManagement.resumeCommands = ''
-    ${pkgs.systemd}/bin/systemctl restart zerotierone.service
-  '';
+      };
+    };
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    initrd.availableKernelModules = [ "xhci_pci" "uas" "usbhid" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+    initrd.kernelModules = [ "xhci_pci" "uas" "usbhid" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+    kernelModules = [ "tp_smapi" "acpi_call" ];
+    extraModulePackages = with config.boot; [ kernelPackages.tp_smapi kernelPackages.acpi_call ];
+    extraModprobeConfig = ''
+      			  options acpi ec_no_wakeup=1
+      			  options thinkpad_acpi fan_control=1
+      		  '';
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
 
   networking = {
     # wlan0 is better b/c this is supposed to boot on different machines, and
     # consistent naming gets in the way b/c names are consistently different.
     usePredictableInterfaceNames = false;
-    hostName        = "chatham";
+    hostName = "chatham";
     wireless = {
       enable = true;
       interfaces = [ "wlan0" ];
@@ -120,13 +79,12 @@
     "/home/data" = { device = "/dev/yuking/data";            fsType = "ext4"; };
   };
 
-  nix.settings.max-jobs                     = lib.mkDefault 12;
+  nix.settings.max-jobs = lib.mkDefault 12;
 
   # The NixOS release to be compatible with for stateful data such as databases.
   system = {
-    stateVersion       = "21.11";
+    stateVersion = "21.11";
     autoUpgrade.enable = false;
   };
 
 }
-
