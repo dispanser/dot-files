13bd1d97b2c280c3eaf6cd92ede21416e801d622
Thomas Peiselt <pi@kulturguerilla.org>
2026-01-23 08:28:44 +0100

    increase wait time for keyboard discovery to fix keyboard detection

--- nixos-systems/keyboard.nix
+++ nixos-systems/keyboard.nix
@@ -2,6 +2,9 @@
 {
   services.kanata = {
     enable = true;
-    keyboards.piantor.configFile = ../kanata/piantor_thinkpad.kbd;
+    keyboards.piantor = {
+      configFile = ../kanata/piantor_thinkpad.kbd;
+      extraArgs = [ "--wait-device-ms" "3000" ];
+    };
   };
 }
