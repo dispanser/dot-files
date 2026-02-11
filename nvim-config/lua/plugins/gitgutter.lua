1f81934bef32b28ad28992e40a31e4b05fde441b
Thomas Peiselt <pi@kulturguerilla.org>
2026-02-11 14:32:02 +0100

    gitgutter keybinding to stage entire file

--- nvim-config/lua/plugins/gitgutter.lua
+++ nvim-config/lua/plugins/gitgutter.lua
@@ -2,6 +2,7 @@ return {
   'airblade/vim-gitgutter',
   keys = {
     {  mode = {'v', 'n'}, '<leader>ga', ':GitGutterStageHunk<CR>', desc = "git stage hunk" },
+    {  mode = {'n'}, '<leader>gA', ':Git add %<CR>', desc = "git stage all hunks in buffer" },
     {  '<leader>gn', ':GitGutterNextHunk<CR>', desc = "git goto next hunk" },
     {  '<leader>gp', ':GitGutterPrevHunk<CR>', desc = "git goto previous hunk" },
   },
