f99940ac42efb464c3a4b076f26541a10ea76bd3
Thomas Peiselt <pi@kulturguerilla.org>
2026-02-11 14:40:49 +0100

    Add gitgutter fold keybinding

--- nvim-config/lua/plugins/gitgutter.lua
+++ nvim-config/lua/plugins/gitgutter.lua
@@ -1,10 +1,11 @@
 return {
   'airblade/vim-gitgutter',
   keys = {
-    {  mode = {'v', 'n'}, '<leader>ga', ':GitGutterStageHunk<CR>', desc = "git stage hunk" },
-    {  mode = {'n'}, '<leader>gA', ':Git add %<CR>', desc = "git stage all hunks in buffer" },
-    {  '<leader>gn', ':GitGutterNextHunk<CR>', desc = "git goto next hunk" },
-    {  '<leader>gp', ':GitGutterPrevHunk<CR>', desc = "git goto previous hunk" },
+    { mode = {'v', 'n'}, '<leader>ga', ':GitGutterStageHunk<CR>', desc = "git stage hunk" },
+    { mode = {'n'}, '<leader>gA', ':Git add %<CR>', desc = "git stage all hunks in buffer" },
+    { '<leader>gn', ':GitGutterNextHunk<CR>', desc = "git goto next hunk" },
+    { '<leader>gp', ':GitGutterPrevHunk<CR>', desc = "git goto previous hunk" },
+    { '<leader>gf', ':GitGutterFold<CR>', desc = "git fold toggle" },
   },
   init = function()
     vim.g.gitgutter_map_keys = 0
